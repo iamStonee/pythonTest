c64bcd8f9ad3d3e1066ddd869385b55b5fe4d715
Jo Liu <joycelnl@gmail.com>
2020-01-07 11:14:24 +0800

    Sync directory tree to S3

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,6 +1,8 @@
 import boto3
 import click
+from pathlib import Path
 from botocore.exceptions import ClientError
+import mimetypes
 
 session = boto3.Session(profile_name='pythonAuthomation')
 s3 = session.resource('s3')
@@ -67,10 +69,37 @@ def setup_bucket(bucket):
     url="https://%s.s3-website-ap-southeast-2.amazonaws.com" % s3_bucket.name
     print(url)
 
+def upload_file(s3_bucket, path, key):
+    "Upload files in an s3 bucket"
+    # set default value of content_type
+    content_type = mimetypes.guess_type(key)[0] or 'text/plain'
+    s3_bucket.upload_file(
+        path,
+        key,
+        ExtraArgs={'ContentType': 'text/html'}
+    )
 
 
-##def upload_file('htmlfile','nameoffile',ExtraArgs='contenttype'):
-##    "Upload files in an s3 bucket"
+@cli.command('sync')
+@click.argument('pathname', type=click.Path(exists=True))
+@click.argument('bucket')
+def sync(pathname, bucket):
+    "Sync contents of PATHNAME to BUCKET"
+#    print(Path(pathname).expanduser().resolve())
+    s3_bucket= s3.Bucket(bucket)
+
+    root = Path(pathname).expanduser().resolve()
+#    print(root)
+
+    def handle_directory(target):
+        for p in target.iterdir():
+             if p.is_dir(): handle_directory(p)
+             if p.is_file():
+                upload_file(s3_bucket, str(p), str(p.relative_to(root)))
+                 #print("Path: {}\n Key: {}".format(p, p.relative_to(root)))
+
+    handle_directory(root)
+
 
 
 if __name__ == '__main__':
